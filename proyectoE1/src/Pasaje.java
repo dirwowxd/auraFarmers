9dbfb67609971c8fde597efc8f7a59651891cf4b
Benjamin Andrade <benjaarv>
2026-04-07 17:47:16 -0400

    Avance Pasaje

---
+++ proyectoE1/src/Pasaje.java
@@ -0,0 +1,28 @@
+public class Pasaje {
+    private int numero;
+    private int asiento;
+
+    public Pasaje(int asiento, Viaje viaje, Pasajero pasajero, Venta venta) {
+
+    }
+
+    public int getNumero (){
+        return
+    }
+
+    public int getAsiento() {
+        return asiento;
+    }
+
+    public Viaje getViaje (){
+
+    }
+
+    public Pasajero getPasajero (){
+
+    }
+
+    public Venta getVenta (){
+
+    }
+}
