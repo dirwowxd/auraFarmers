7aff376bde2e310763ada1e2927c5390458e22e0
Benjamin Andrade <benjaarv>
2026-04-07 17:31:14 -0400

    Avance en las dos clases pasaporte rut

--- proyectoE1/src/Rut.java
+++ proyectoE1/src/Rut.java
@@ -1,3 +1,20 @@
 public class Rut {
-    private int Numero;
+    private int numero;
+    private char dv;
+
+    private Rut(int numero, char dv) {
+        this.numero = numero;
+        this.dv = dv;
+    }
+    public int getNumero() {
+        return numero;
+    }
+
+    public char getDv() {
+        return dv;
+    }
+
+    public static Rut of(int numero, char dv) {
+        return new Rut(numero, dv);
+    }
 }
