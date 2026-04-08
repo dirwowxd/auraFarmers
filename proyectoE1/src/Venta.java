8e8b85f3c0e3dc41a173cad858d746e57dc20506
cristobalPoblete <cristobal.poblete2501@alumnos.ubiobio.cl>
2026-04-08 11:10:05 -0400

    Venta finalizado c.r

--- proyectoE1/src/Venta.java
+++ proyectoE1/src/Venta.java
@@ -1,17 +1,21 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 public class Venta {
     private String idDocumento;
     private TipoDocumento Tipo;
     private LocalDate Fecha;
-
-    ArrayList<Pasaje> pasaje = new ArrayList<>();
+    private Cliente cliente;
+    private Pasaje[] pasajes;
+    private int CantidadPasajes;
 
     public Venta (String idDocumento, TipoDocumento Tipo, LocalDate Fecha, Cliente cliente)  {
       this.idDocumento = idDocumento;
       this.Tipo = Tipo;
       this.Fecha = Fecha;
+      this.cliente = cliente;
+
+      this.pasajes = new Pasaje[40];
+      this.CantidadPasajes = 0;
     }
 
     public String getIdDocumento() {
@@ -22,14 +26,41 @@ public class Venta {
         return Tipo;
     }
 
+    public LocalDate getFecha() {
+        return Fecha;
+    }
 
+    public Cliente getCliente() {
+        return cliente;
+    }
 
+    public void createPasaje(int asiento, Viaje viaje, Pasajero pasajero) {
+        if (this.CantidadPasajes < this.pasajes.length) {
 
+            Pasaje PasajeNuevo = new Pasaje(asiento,viaje,pasajero,this);
+            Pasaje[CantidadPasajes] = nuevoPasaje;
+            this.CantidadPasajes++;
+        } else {
+            System.out.println("//ERROR//");
+            System.out.println("La cantidad de pasajes de la venta supera el limite del bus");
+        }
+    }
 
+    public Pasaje[] getPasajes() {
+        Pasaje[] pasajesVendidos = new Pasaje[CantidadPasajes];
 
+        for (int i = 0; i < CantidadPasajes; i++) {
+            pasajesVendidos[i] = pasajes[i];
+        }
+        return pasajesVendidos;
+    }
 
-
-
-
+    public int getMonto () {
+        int montoTotal = 0;
+        for (int i = 0; i < CantidadPasajes; i++) {
+            montoTotal += pasajes[i].getViaje().getPrecio();
+        }
+        return montoTotal;
+    }
 
 }
