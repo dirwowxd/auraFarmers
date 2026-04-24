3ad9a857bb7497c0845a390fb48a2989a1f2bce0
dirwowxd <vicesepulveda2006@gmail.com>
2026-04-24 13:23:02 -0400

    error con los merge tuve que subirlo denuevo 2

--- proyectoE1/src/SistemaVentaPasajes.java
+++ proyectoE1/src/SistemaVentaPasajes.java
@@ -1,4 +1,3 @@
-import org.w3c.dom.ls.LSOutput;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -57,18 +56,18 @@ public class SistemaVentaPasajes {
 
     public boolean createViaje(LocalDate fecha, LocalTime hora, int precio, String patenteBus) {
         for (int i = 0; i < viajes.size(); i++) {
-        //falta clase viaje
+            //falta clase viaje
         }
         return true;
     }
 
-    public boolean iniciaVenta(String idDocumento, TipoDocumento tipo, LocalDate fechaVenta,idPersona idCliente) {
+    public boolean iniciaVenta(String idDocumento, TipoDocumento tipo, LocalDate fechaVenta, idPersona idCliente) {
         boolean existeCliente = false;
-        Cliente clienteComprador=null;
+        Cliente clienteComprador = null;
         for (Cliente cliente : clientes) {
             if (idCliente.equals(cliente.getIdPersona())) {
                 existeCliente = true;
-                clienteComprador=cliente;
+                clienteComprador = cliente;
                 break;
             }
         }
@@ -81,49 +80,75 @@ public class SistemaVentaPasajes {
             }
         }
 
-        Venta nuevaVenta= new Venta(idDocumento, tipo, fechaVenta, clienteComprador);
+        Venta nuevaVenta = new Venta(idDocumento, tipo, fechaVenta, clienteComprador);
         ventas.add(nuevaVenta);
         return true;
     }
 
     public String[][] getHorariosDisponibles(LocalDate fechaViaje) {
-        return null;//hacer
+        int contador = 0;
+        for (Viaje viaje : viajes) {
+            if (viaje.getFecha().equals(fechaViaje)) {
+                contador++;
+            }
+        }
+
+        String[][] datos = new String[contador][4];
+        int i = 0;
+
+        for (Viaje viaje : viajes) {
+            if (viaje.getFecha().equals(fechaViaje)) {
+                datos[i][0] = viaje.getBus().getPatente();
+                datos[i][1] = viaje.getHora().toString();
+                datos[i][2] = String.valueOf(viaje.getPrecio());
+                datos[i][3] = String.valueOf(viaje.getNroAsientosDisponibles());
+                i++;
+            }
+        }
+        return datos;
     }
-    public String[][]listAsientosDeViaje(LocalDate fecha, LocalTime hora, String patenteBus) {
-        return null;//hacer
+
+    public String[][] listAsientosDeViaje (LocalDate fecha, LocalTime hora, String patenteBus){
+        Viaje viaje = findViaje(fecha.toString(), hora.toString(), patenteBus);
+        if (viaje == null) {
+            return new String[0][0];
+        }
+        return viaje.getAsientos();
     }
-    public int getMontoVenta(String idDocumento, TipoDocumento tipo) {
+
+    public int getMontoVenta (String idDocumento, TipoDocumento tipo){
         return 0;//hacer
     }
-    public String getNombrePasajero(idPersona idPasajero) {
-        return null; //hacer
+    public String getNombrePasajero (idPersona idPasajero){
+        Pasajero pasajero = findPasajero(idPasajero);
+        if (pasajero == null) {
+            return null;
+        }
+        return pasajero.getNombreCompleto().getNombres();
     }
-    public boolean vendePasaje(String idDoc, LocalDate fecha, LocalTime hora, String patenteBus, idPersona idPasajero) {
-        return false; //hacer
 
-    }
-    public String[][] listVentas(){
-        return null; //hacer
-
-    }
-    public String[][] listViajes() {
+    public String[][] listVentas () {
         return null; //hacer
 
     }
-    public String[][] listPasajeros(LocalDate fecha, LocalTime hora, String patenteBus) {
+    public String[][] listViajes () {
         return null; //hacer
-    }
-    public Cliente findCliente(idPersona id) {
-        return null; //hacer
-    }
-    public Venta findVenta(String idDocumento, TipoDocumento tipoDocumento) {
-        return null; //hacer
-    }
-    public Viaje findViaje(String fecha, String hora, String patenteBus) {
-        return null; //hacer
-    }
-    public Pasajero findPasajero(idPersona idPasajero) {
-        return null; //hacer
-    }
 
+    }
+    public String[][] listPasajeros (LocalDate fecha, LocalTime hora, String patenteBus){
+        return null; //hacer
+    }
+    public Cliente findCliente (idPersona id){
+        return null; //hacer
+    }
+    public Venta findVenta (String idDocumento, TipoDocumento tipoDocumento){
+        return null; //hacer
+    }
+    public Viaje findViaje (String fecha, String hora, String patenteBus){
+        return null; //hacer
+    }
+    public Pasajero findPasajero (idPersona idPasajero){
+        return null; //hacer
+    }
+    //
 }
