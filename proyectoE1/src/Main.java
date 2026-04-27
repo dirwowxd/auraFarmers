1c96e0ed6192c74c81d42d427990c8ac5bb69d3d
cristobalPoblete <cristobal.poblete2501@alumnos.ubiobio.cl>
2026-04-26 21:18:29 -0400

    finalizado las listas dentro del main con el formato del word del avance

--- proyectoE1/src/Main.java
+++ proyectoE1/src/Main.java
@@ -89,13 +89,105 @@ public class Main {
     private void vendePasajes() {
 
     }
-    private void listPasajerosViaje() {
+    private void listPasajerosViaje(String [][] matrizPasajeros, String idViaje) {
+
+        if (matrizPasajeros == null ||  matrizPasajeros.length == 0) {
+            System.out.println("No hay pasajeros registrados para el viaje: "+ idViaje);
+            return;
+        }
+
+        System.out.println("°*Listado de Pasajeros::*°");
+        System.out.println("                (Viaje: " + idViaje + ")");
+        System.out.println();
+
+        System.out.println("*---------*-----------------*------------------------------------------*-----------------*");
+
+        System.out.printf("| %-7s | %-15s | %-40s | %-15s |\n",
+                "ASIENTO", "RUT/PASAPORTE", "NOMBRE CLIENTE", "TIPO PASAJERO");
+
+        System.out.println("|---------+-----------------+------------------------------------------+-----------------|");
+
+        for (int i = 0; i < matrizPasajeros.length; i++) {
+
+            String asiento = matrizPasajeros[i][0];
+            String documento = matrizPasajeros[i][1];
+            String nombre = matrizPasajeros[i][2];
+            String tipo = matrizPasajeros[i][3];
+
+            System.out.printf("| %7s | %-15s | %-40s | %-15s |\n",
+                    asiento, documento, nombre, tipo);
+        }
+
+        System.out.println("*---------*-----------------*------------------------------------------*-----------------*");
+    }
+
+    private void listVentas(String[][] matrizVentas) {
+        if (matrizVentas == null || matrizVentas.length == 0) {
+            System.out.println("No existen ventas registradas en el sistema.");
+            return;
+        }
+
+        System.out.println("°*::Lista de ventas::*°");
+        System.out.println();
+
+        System.out.println("*-------------*-------------*--------------*-----------------*--------------------------------*----------------*---------------*");
+        System.out.printf("| %-11s | %-11s | %-12s | %-15s | %-30s | %-14s | %-13s |\n",
+                "ID DOCUMENTO", "TIPO DOCUMENTO", "FECHA", "RUT/PASAPORTE", "CLIENTE", "CANT. BOLETOS", "TOTAL VENTA");
+
+        System.out.println("|-------------+-------------+--------------+-----------------+--------------------------------+----------------+---------------|");
+
+        for (int i = 0; i < matrizVentas.length; i++) {
+
+            String idDoc = matrizVentas[i][0];
+            String tipoDoc = matrizVentas[i][1];
+            String fecha = matrizVentas[i][2];
+            String rut = matrizVentas[i][3];
+            String cliente = matrizVentas[i][4];
+            String cantBoletos = matrizVentas[i][5];
+            String totalVenta = matrizVentas[i][6];
+
+
+            System.out.printf("| %11s | %-11s | %-12s | %-15s | %-30s | %14s | %13s |\n",
+                    idDoc, tipoDoc, fecha, rut, cliente, cantBoletos, totalVenta);
+        }
+
+        System.out.println("*-------------*-------------*--------------*-----------------*--------------------------------*----------------*---------------*");
+
+
 
     }
-    private void listVentas() {
 
-    }
-    private void listViajes() {
+    private void listViajes(String[][] matrizViajes) {
+
+        if (matrizViajes == null || matrizViajes.length == 0) {
+            System.out.println("No hay viajes programados dentro del sistema.");
+            return;
+        }
+
+        System.out.println("°*Listado de Viajes*°");
+        System.out.println();
+
+        System.out.println("*------------*--------------*----------*-----------------*-----------------*---------------*");
+
+        System.out.printf("| %-10s | %-12s | %-8s | %-15s | %-15s | %-13s |\n",
+                "ID VIAJE", "FECHA", "HORA", "ORIGEN", "DESTINO", "PATENTE BUS");
+
+        System.out.println("|------------+--------------+----------+-----------------+-----------------+---------------|");
+
+        for (int i = 0; i < matrizViajes.length; i++) {
+
+            String idViaje = matrizViajes[i][0];
+            String fecha = matrizViajes[i][1];
+            String hora = matrizViajes[i][2];
+            String origen = matrizViajes[i][3];
+            String destino = matrizViajes[i][4];
+            String patente = matrizViajes[i][5];
+
+            System.out.printf("| %-10s | %-12s | %-8s | %-15s | %-15s | %-13s |\n",
+                    idViaje, fecha, hora, origen, destino, patente);
+        }
+
+        System.out.println("*------------*--------------*----------*-----------------*-----------------*---------------*");
 
     }
 }
