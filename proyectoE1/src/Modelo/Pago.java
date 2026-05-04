c93f7c48485844ce1d4f46c83daa4ac01c13dd3c
cristobalPoblete <cristobal.poblete2501@alumnos.ubiobio.cl>
2026-05-04 15:49:53 -0400

    terminado todos los pagos

--- proyectoE1/src/Modelo/Pago.java
+++ proyectoE1/src/Modelo/Pago.java
@@ -1,4 +1,14 @@
 package Modelo;
 
 public class Pago {
+    private int Monto;
+
+    public Pago(int Monto){
+        this.Monto = Monto;
+    }
+
+    public int getMonto(){
+        return Monto;
+    }
+
 }
