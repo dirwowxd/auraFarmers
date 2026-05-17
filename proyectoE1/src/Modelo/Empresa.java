23b7d5f7a5ebee43f54130fbc298ac65efeca7a3
Vicente <vicesepulveda2006@gmail.com>
2026-05-17 19:47:38 -0400

    agregue constructor de Tripulante y termine la clase de Empresa

--- proyectoE1/src/Modelo/Empresa.java
+++ proyectoE1/src/Modelo/Empresa.java
@@ -9,6 +9,7 @@ import utilidades.Rut;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Empresa {
     private Rut rut;
@@ -53,7 +54,7 @@ public class Empresa {
     public void addBuses(Bus bus) {
         for (Bus busAux : buses) {
             if (busAux.getPatente().equals(bus.getPatente())) {
-                throw new SistemaVentaPasajesException("Ya existe un bus con la patente " + busAux);
+                throw new SistemaVentaPasajesException("Ya existe un bus con la patente " + busAux.getPatente());
             }
         }
         buses.add(bus);
@@ -65,14 +66,50 @@ public class Empresa {
 
 
     public boolean addConductor(IdPersona id, Nombre nom, Direccion direccion ) {
+        for (Conductor conductorAux : conductores) {
+            if (conductorAux.getIdPersona().equals(id)) { // esta malo hasta que se cree el metodo getter de getIdPersona en la clase Conductor //17-05-2026
+                return false;
+            }
+        }
+
         for (Tripulante tripulante: tripulantes){// primero recorro para saber los tripulantes
             if (tripulante.getIdPersona().equals(id)){ //aqui comparo los id's de los tripulantes si son iguales
                 return false;
             }
         }
+
         Conductor conductor= new Conductor(id, nom, direccion); // esta erronea hasta que Benja haga la clase de Conductor 14/05/2026
-        tripulantes.add(conductor); //tambien da error debido a que aun no se termina la clase tripulante
+        conductores.add(conductor);
         return true;
 
+    }
+    public boolean addAuxiliar (IdPersona id, Nombre nom, Direccion direccion) {
+        for (Conductor conductorAux : conductores) {
+            if (conductorAux.getIdPersona().equals(id)){ // lo mismo que en la linea 68
+                return false;
+            }
+        }
+
+        for (Tripulante tripulante: tripulantes){
+            if (tripulante.getIdPersona().equals(id)){
+                return false;
+            }
+        }
+
+        Auxiliar auxiliar= new Auxiliar(id, nom,direccion);
+        tripulantes.add(auxiliar);
+        return true;
+    }
+    public Tripulante[] getTripulantes() {
+        return tripulantes.toArray(new Tripulante[0]);
+    }
+    public Venta[] getventas() {
+        ArrayList<Venta> ventas= new ArrayList<>();
+        for (Bus busAux : buses) {
+            for (Viaje viajeAux : busAux.getViajes()) {
+                Collections.addAll(ventas, viajeAux.getVentas()); // intellij lo hizo y basicamente ahorra mas tiempo que con el .add al ser un ciclo for de 3
+            }
+        }
+        return ventas.toArray(new Venta[0]);
     }
 }
--- proyectoE1/src/Modelo/Tripulante.java
+++ proyectoE1/src/Modelo/Tripulante.java
@@ -3,8 +3,8 @@ package Modelo;
 import utilidades.IdPersona;
 import utilidades.Nombre;
 
-public class Tripulante extends Persona{
-    public Tripulante(IdPersona idPersona, Nombre nombreCompleto) {
-        super(idPersona, nombreCompleto);
+public class Tripulante extends Persona{ //hacer
+    public Tripulante(IdPersona idPersona, Nombre nombreCompleto, String telefono) {
+        super(idPersona, nombreCompleto, telefono);
     }
 }
