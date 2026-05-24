38a4e6c3df041e6448b71ed1f89dc8cb0c19e922
Benjamin Andrade <benjaarv>
2026-05-24 17:30:53 -0400

    Termine clases: terminal, tripulante y direccion. Avance en bus.

--- proyectoE1/src/Modelo/Terminal.java
+++ proyectoE1/src/Modelo/Terminal.java
@@ -1,4 +1,67 @@
 package Modelo;
 
+import utilidades.Direccion;
+
 public class Terminal {
+
+    private String nombre;
+    private String comuna;
+    private Direccion direccion;
+
+    public Terminal(String nombre, String comuna, Direccion direccion) {
+
+        this.nombre = nombre;
+        this.comuna = comuna;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setComuna(String comuna) {
+        this.comuna = comuna;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+
+        return "----------------------------------------\n" +
+                " DATOS TERMINAL\n" +
+                "----------------------------------------\n" +
+                " Nombre    : " + nombre + "\n" +
+                " Comuna    : " + comuna + "\n" +
+                " Dirección : " + direccion + "\n" +
+                "----------------------------------------";
+    }
+
+    @Override
+    public boolean equals(Object otro) {
+
+        if (this == otro) return true;
+
+        if (otro == null || getClass() != otro.getClass()) {
+            return false;
+        }
+
+        Terminal terminal = (Terminal) otro;
+
+        return nombre.equals(terminal.nombre);
+    }
 }
--- proyectoE1/src/utilidades/Direccion.java
+++ proyectoE1/src/utilidades/Direccion.java
@@ -1,5 +1,48 @@
 package utilidades;
 
 public class Direccion {
+    private String calle;
+    private int numero;
 
+    public Direccion(String calle, int numero) {
+        this.calle = calle;
+        this.numero = numero;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return calle + " " + numero;
+    }
+
+    @Override
+    public boolean equals(Object otro) {
+        if (this == otro) {
+            return true;
+        }
+
+        if (otro == null || getClass() != otro.getClass()) {
+            return false;
+        }
+
+        Direccion direccion = (Direccion) otro;
+
+        return numero == direccion.numero &&
+                calle.equals(direccion.calle);
+    }
 }
