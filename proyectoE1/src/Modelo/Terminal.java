7dd62e12321cbc256d37bb67a058d88d0027a8a2
Benjamin Andrade <benjaarv>
2026-05-25 15:50:40 -0400

    Subidos al main bus, terminal,tripulante.

--- proyectoE1/src/Modelo/Terminal.java
+++ proyectoE1/src/Modelo/Terminal.java
@@ -1,4 +1,53 @@
 package Modelo;
 
+
+import utilidades.Direccion;
+
+
 public class Terminal {
+
+
+    private String nombre;
+    private Direccion direccion;
+
+
+    public Terminal(String nombre, Direccion direccion) {
+
+
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+
+    @Override
+    public String toString() {
+
+
+        return "----------------------------------------\n" +
+                " DATOS TERMINAL\n" +
+                "----------------------------------------\n" +
+                " Nombre    : " + nombre + "\n" +
+                " Dirección : " + direccion + "\n" +
+                "----------------------------------------";
+    }
 }
